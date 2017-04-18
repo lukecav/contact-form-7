3c9bea7ced360d58a1092c35d26cc85ef93d507f
Luke Cavanagh <lukecav@users.noreply.github.com>
2017-04-18 12:34:02 -0600

    Update capabilities.php

--- includes/capabilities.php
+++ includes/capabilities.php
@@ -1,16 +1,17 @@
 <?php
 
-add_filter( 'map_meta_cap', 'wpcf7_map_meta_cap', 10, 4 );
+add_filter( 'map_meta_cap', 'wpcf8_map_meta_cap', 10, 4 );
 
-function wpcf7_map_meta_cap( $caps, $cap, $user_id, $args ) {
+function wpcf8_map_meta_cap( $caps, $cap, $user_id, $args ) {
 	$meta_caps = array(
-		'wpcf7_edit_contact_form' => WPCF7_ADMIN_READ_WRITE_CAPABILITY,
-		'wpcf7_edit_contact_forms' => WPCF7_ADMIN_READ_WRITE_CAPABILITY,
-		'wpcf7_read_contact_forms' => WPCF7_ADMIN_READ_CAPABILITY,
-		'wpcf7_delete_contact_form' => WPCF7_ADMIN_READ_WRITE_CAPABILITY,
-		'wpcf7_manage_integration' => 'manage_options' );
+		'wpcf8_edit_contact_form' => WPCF8_ADMIN_READ_WRITE_CAPABILITY,
+		'wpcf8_edit_contact_forms' => WPCF8_ADMIN_READ_WRITE_CAPABILITY,
+		'wpcf8_read_contact_forms' => WPCF8_ADMIN_READ_CAPABILITY,
+		'wpcf8_delete_contact_form' => WPCF8_ADMIN_READ_WRITE_CAPABILITY,
+		'wpcf8_manage_integration' => 'manage_options',
+	);
 
-	$meta_caps = apply_filters( 'wpcf7_map_meta_cap', $meta_caps );
+	$meta_caps = apply_filters( 'wpcf8_map_meta_cap', $meta_caps );
 
 	$caps = array_diff( $caps, array_keys( $meta_caps ) );
 
