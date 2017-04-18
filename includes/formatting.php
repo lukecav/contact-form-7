d4fb00df8ce503324f5d17e4c5e9eeb9eba3c69e
Luke Cavanagh <lukecav@users.noreply.github.com>
2017-04-18 12:39:52 -0600

    Update formatting.php

--- includes/formatting.php
+++ includes/formatting.php
@@ -1,6 +1,6 @@
 <?php
 
-function wpcf7_autop( $pee, $br = 1 ) {
+function wpcf8_autop( $pee, $br = 1 ) {
 	if ( trim( $pee ) === '' ) {
 		return '';
 	}
@@ -8,13 +8,14 @@ function wpcf7_autop( $pee, $br = 1 ) {
 	$pee = $pee . "\n"; // just to make things a little easier, pad the end
 	$pee = preg_replace( '|<br />\s*<br />|', "\n\n", $pee );
 	// Space things out a little
-	/* wpcf7: remove select and input */
+	/* wpcf8: remove select and input */
 	$allblocks = '(?:table|thead|tfoot|caption|col|colgroup|tbody|tr|td|th|div|dl|dd|dt|ul|ol|li|pre|form|map|area|blockquote|address|math|style|p|h[1-6]|hr|fieldset|legend|section|article|aside|hgroup|header|footer|nav|figure|figcaption|details|menu|summary)';
 	$pee = preg_replace( '!(<' . $allblocks . '[^>]*>)!', "\n$1", $pee );
 	$pee = preg_replace( '!(</' . $allblocks . '>)!', "$1\n\n", $pee );
 
-	/* wpcf7: take care of [response] and [recaptcha] tag */
-	$pee = preg_replace( '!(\[(?:response|recaptcha)[^]]*\])!',
+	/* wpcf8: take care of [response], [recaptcha], and [hidden] tags */
+	$block_hidden_form_tags = '(?:response|recaptcha|hidden)';
+	$pee = preg_replace( '!(\[' . $block_hidden_form_tags . '[^]]*\])!',
 		"\n$1\n\n", $pee );
 
 	$pee = str_replace( array( "\r\n", "\r" ), "\n", $pee ); // cross-platform newlines
@@ -42,28 +43,37 @@ function wpcf7_autop( $pee, $br = 1 ) {
 	$pee = preg_replace( '!<p>\s*(</?' . $allblocks . '[^>]*>)!', "$1", $pee );
 	$pee = preg_replace( '!(</?' . $allblocks . '[^>]*>)\s*</p>!', "$1", $pee );
 
-	/* wpcf7: take care of [response] and [recaptcha] tag */
-	$pee = preg_replace( '!<p>\s*(\[(?:response|recaptcha)[^]]*\])!',
+	/* wpcf8: take care of [response], [recaptcha], and [hidden] tag */
+	$pee = preg_replace( '!<p>\s*(\[' . $block_hidden_form_tags . '[^]]*\])!',
 		"$1", $pee );
-	$pee = preg_replace( '!(\[(?:response|recaptcha)[^]]*\])\s*</p>!',
+	$pee = preg_replace( '!(\[' . $block_hidden_form_tags . '[^]]*\])\s*</p>!',
 		"$1", $pee );
 
 	if ( $br ) {
-		/* wpcf7: add textarea */
+		/* wpcf8: add textarea */
 		$pee = preg_replace_callback( '/<(script|style|textarea).*?<\/\\1>/s', create_function( '$matches', 'return str_replace("\n", "<WPPreserveNewline />", $matches[0]);' ), $pee );
 		$pee = preg_replace( '|(?<!<br />)\s*\n|', "<br />\n", $pee ); // optionally make line breaks
 		$pee = str_replace( '<WPPreserveNewline />', "\n", $pee );
+
+		/* wpcf8: remove extra <br /> just added before [response], [recaptcha], and [hidden] tags */
+		$pee = preg_replace( '!<br />\n(\[' . $block_hidden_form_tags . '[^]]*\])!',
+			"\n$1", $pee );
 	}
+
 	$pee = preg_replace( '!(</?' . $allblocks . '[^>]*>)\s*<br />!', "$1", $pee );
 	$pee = preg_replace( '!<br />(\s*</?(?:p|li|div|dl|dd|dt|th|pre|td|ul|ol)[^>]*>)!', '$1', $pee );
-	if ( strpos( $pee, '<pre' ) !== false )
-		$pee = preg_replace_callback( '!(<pre[^>]*>)(.*?)</pre>!is', 'clean_pre', $pee );
+
+	if ( strpos( $pee, '<pre' ) !== false ) {
+		$pee = preg_replace_callback( '!(<pre[^>]*>)(.*?)</pre>!is',
+			'clean_pre', $pee );
+	}
+
 	$pee = preg_replace( "|\n</p>$|", '</p>', $pee );
 
 	return $pee;
 }
 
-function wpcf7_sanitize_query_var( $text ) {
+function wpcf8_sanitize_query_var( $text ) {
 	$text = wp_unslash( $text );
 	$text = wp_check_invalid_utf8( $text );
 
@@ -79,69 +89,80 @@ function wpcf7_sanitize_query_var( $text ) {
 	return $text;
 }
 
-function wpcf7_strip_quote( $text ) {
+function wpcf8_strip_quote( $text ) {
 	$text = trim( $text );
 
-	if ( preg_match( '/^"(.*)"$/', $text, $matches ) )
+	if ( preg_match( '/^"(.*)"$/s', $text, $matches ) ) {
 		$text = $matches[1];
-	elseif ( preg_match( "/^'(.*)'$/", $text, $matches ) )
+	} elseif ( preg_match( "/^'(.*)'$/s", $text, $matches ) ) {
 		$text = $matches[1];
+	}
 
 	return $text;
 }
 
-function wpcf7_strip_quote_deep( $arr ) {
-	if ( is_string( $arr ) )
-		return wpcf7_strip_quote( $arr );
+function wpcf8_strip_quote_deep( $arr ) {
+	if ( is_string( $arr ) ) {
+		return wpcf8_strip_quote( $arr );
+	}
 
 	if ( is_array( $arr ) ) {
 		$result = array();
 
-		foreach ( $arr as $key => $text )
-			$result[$key] = wpcf7_strip_quote_deep( $text );
+		foreach ( $arr as $key => $text ) {
+			$result[$key] = wpcf8_strip_quote_deep( $text );
+		}
 
 		return $result;
 	}
 }
 
-function wpcf7_normalize_newline( $text, $to = "\n" ) {
-	if ( ! is_string( $text ) )
+function wpcf8_normalize_newline( $text, $to = "\n" ) {
+	if ( ! is_string( $text ) ) {
 		return $text;
+	}
 
 	$nls = array( "\r\n", "\r", "\n" );
 
-	if ( ! in_array( $to, $nls ) )
+	if ( ! in_array( $to, $nls ) ) {
 		return $text;
+	}
 
 	return str_replace( $nls, $to, $text );
 }
 
-function wpcf7_normalize_newline_deep( $arr, $to = "\n" ) {
+function wpcf8_normalize_newline_deep( $arr, $to = "\n" ) {
 	if ( is_array( $arr ) ) {
 		$result = array();
 
-		foreach ( $arr as $key => $text )
-			$result[$key] = wpcf7_normalize_newline_deep( $text, $to );
+		foreach ( $arr as $key => $text ) {
+			$result[$key] = wpcf8_normalize_newline_deep( $text, $to );
+		}
 
 		return $result;
 	}
 
-	return wpcf7_normalize_newline( $arr, $to );
+	return wpcf8_normalize_newline( $arr, $to );
 }
 
-function wpcf7_strip_newline( $str ) {
+function wpcf8_strip_newline( $str ) {
 	$str = (string) $str;
 	$str = str_replace( array( "\r", "\n" ), '', $str );
 	return trim( $str );
 }
 
-function wpcf7_canonicalize( $text ) {
+function wpcf8_canonicalize( $text, $strto = 'lower' ) {
 	if ( function_exists( 'mb_convert_kana' )
 	&& 'UTF-8' == get_option( 'blog_charset' ) ) {
 		$text = mb_convert_kana( $text, 'asKV', 'UTF-8' );
 	}
 
-	$text = strtolower( $text );
+	if ( 'lower' == $strto ) {
+		$text = strtolower( $text );
+	} elseif ( 'upper' == $strto ) {
+		$text = strtoupper( $text );
+	}
+
 	$text = trim( $text );
 	return $text;
 }
@@ -157,50 +178,154 @@ function wpcf7_canonicalize( $text ) {
  *
  * @return bool True if it is a valid name, false if not.
  */
-function wpcf7_is_name( $string ) {
+function wpcf8_is_name( $string ) {
 	return preg_match( '/^[A-Za-z][-A-Za-z0-9_:.]*$/', $string );
 }
 
-function wpcf7_sanitize_unit_tag( $tag ) {
+function wpcf8_sanitize_unit_tag( $tag ) {
 	$tag = preg_replace( '/[^A-Za-z0-9_-]/', '', $tag );
 	return $tag;
 }
 
-function wpcf7_is_email( $email ) {
+function wpcf8_is_email( $email ) {
 	$result = is_email( $email );
-	return apply_filters( 'wpcf7_is_email', $result, $email );
+	return apply_filters( 'wpcf8_is_email', $result, $email );
 }
 
-function wpcf7_is_url( $url ) {
+function wpcf8_is_url( $url ) {
 	$result = ( false !== filter_var( $url, FILTER_VALIDATE_URL ) );
-	return apply_filters( 'wpcf7_is_url', $result, $url );
+	return apply_filters( 'wpcf8_is_url', $result, $url );
 }
 
-function wpcf7_is_tel( $tel ) {
-	$result = preg_match( '/^[+]?[0-9() -]*$/', $tel );
-	return apply_filters( 'wpcf7_is_tel', $result, $tel );
+function wpcf8_is_tel( $tel ) {
+	$result = preg_match( '%^[+]?[0-9()/ -]*$%', $tel );
+	return apply_filters( 'wpcf8_is_tel', $result, $tel );
 }
 
-function wpcf7_is_number( $number ) {
+function wpcf8_is_number( $number ) {
 	$result = is_numeric( $number );
-	return apply_filters( 'wpcf7_is_number', $result, $number );
+	return apply_filters( 'wpcf8_is_number', $result, $number );
 }
 
-function wpcf7_is_date( $date ) {
+function wpcf8_is_date( $date ) {
 	$result = preg_match( '/^([0-9]{4,})-([0-9]{2})-([0-9]{2})$/', $date, $matches );
 
-	if ( $result )
+	if ( $result ) {
 		$result = checkdate( $matches[2], $matches[3], $matches[1] );
+	}
 
-	return apply_filters( 'wpcf7_is_date', $result, $date );
+	return apply_filters( 'wpcf8_is_date', $result, $date );
 }
 
-function wpcf7_antiscript_file_name( $filename ) {
+function wpcf8_is_mailbox_list( $mailbox_list ) {
+	if ( ! is_array( $mailbox_list ) ) {
+		$mailbox_text = (string) $mailbox_list;
+		$mailbox_text = wp_unslash( $mailbox_text );
+
+		$mailbox_text = preg_replace( '/\\\\(?:\"|\')/', 'esc-quote',
+			$mailbox_text );
+
+		$mailbox_text = preg_replace( '/(?:\".*?\"|\'.*?\')/', 'quoted-string',
+			$mailbox_text );
+
+		$mailbox_list = explode( ',', $mailbox_text );
+	}
+
+	$addresses = array();
+
+	foreach ( $mailbox_list as $mailbox ) {
+		if ( ! is_string( $mailbox ) ) {
+			return false;
+		}
+
+		$mailbox = trim( $mailbox );
+
+		if ( preg_match( '/<(.+)>$/', $mailbox, $matches ) ) {
+			$addr_spec = $matches[1];
+		} else {
+			$addr_spec = $mailbox;
+		}
+
+		if ( ! wpcf8_is_email( $addr_spec ) ) {
+			return false;
+		}
+
+		$addresses[] = $addr_spec;
+	}
+
+	return $addresses;
+}
+
+function wpcf8_is_email_in_domain( $email, $domain ) {
+	$email_list = wpcf8_is_mailbox_list( $email );
+	$domain = strtolower( $domain );
+
+	foreach ( $email_list as $email ) {
+		$email_domain = substr( $email, strrpos( $email, '@' ) + 1 );
+		$email_domain = strtolower( $email_domain );
+		$domain_parts = explode( '.', $domain );
+
+		do {
+			$site_domain = implode( '.', $domain_parts );
+
+			if ( $site_domain == $email_domain ) {
+				continue 2;
+			}
+
+			array_shift( $domain_parts );
+		} while ( $domain_parts );
+
+		return false;
+	}
+
+	return true;
+}
+
+function wpcf8_is_email_in_site_domain( $email ) {
+	if ( wpcf8_is_localhost() ) {
+		return true;
+	}
+
+	$site_domain = strtolower( $_SERVER['SERVER_NAME'] );
+
+	if ( preg_match( '/^[0-9.]+$/', $site_domain ) ) { // 123.456.789.012
+		return true;
+	}
+
+	if ( wpcf8_is_email_in_domain( $email, $site_domain ) ) {
+		return true;
+	}
+
+	$home_url = home_url();
+
+	// for interoperability with WordPress MU Domain Mapping plugin
+	if ( is_multisite() && function_exists( 'domain_mapping_siteurl' ) ) {
+		$domain_mapping_siteurl = domain_mapping_siteurl( false );
+
+		if ( $domain_mapping_siteurl ) {
+			$home_url = $domain_mapping_siteurl;
+		}
+	}
+
+	if ( preg_match( '%^https?://([^/]+)%', $home_url, $matches ) ) {
+		$site_domain = strtolower( $matches[1] );
+
+		if ( $site_domain != strtolower( $_SERVER['SERVER_NAME'] )
+		&& wpcf8_is_email_in_domain( $email, $site_domain ) ) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+function wpcf8_antiscript_file_name( $filename ) {
 	$filename = basename( $filename );
 	$parts = explode( '.', $filename );
 
-	if ( count( $parts ) < 2 )
+	if ( count( $parts ) < 2 ) {
 		return $filename;
+	}
 
 	$script_pattern = '/^(php|phtml|pl|py|rb|cgi|asp|aspx)\d?$/i';
 
@@ -208,21 +333,23 @@ function wpcf7_antiscript_file_name( $filename ) {
 	$extension = array_pop( $parts );
 
 	foreach ( (array) $parts as $part ) {
-		if ( preg_match( $script_pattern, $part ) )
+		if ( preg_match( $script_pattern, $part ) ) {
 			$filename .= '.' . $part . '_';
-		else
+		} else {
 			$filename .= '.' . $part;
+		}
 	}
 
-	if ( preg_match( $script_pattern, $extension ) )
+	if ( preg_match( $script_pattern, $extension ) ) {
 		$filename .= '.' . $extension . '_.txt';
-	else
+	} else {
 		$filename .= '.' . $extension;
+	}
 
 	return $filename;
 }
 
-function wpcf7_mask_password( $text, $length_unmasked = 0 ) {
+function wpcf8_mask_password( $text, $length_unmasked = 0 ) {
 	$length = strlen( $text );
 	$length_unmasked = absint( $length_unmasked );
 
