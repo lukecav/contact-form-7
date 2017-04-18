adcdc525d82fbd4660155e11cca161c9a845c790
Luke Cavanagh <lukecav@users.noreply.github.com>
2017-04-18 12:13:55 -0600

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,26 +1,28 @@
 <?php
 
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) )
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit();
-
-function wpcf7_delete_plugin() {
-	global $wpdb;
-
-	delete_option( 'wpcf7' );
-
-	$posts = get_posts( array(
-		'numberposts' => -1,
-		'post_type' => 'wpcf7_contact_form',
-		'post_status' => 'any' ) );
-
-	foreach ( $posts as $post )
-		wp_delete_post( $post->ID, true );
-
-	$table_name = $wpdb->prefix . "contact_form_7";
-
-	$wpdb->query( "DROP TABLE IF EXISTS $table_name" );
 }
 
-wpcf7_delete_plugin();
+function wpcf8_delete_plugin() {
+	global $wpdb;
 
-?>
+	delete_option( 'wpcf8' );
+
+	$posts = get_posts(
+		array(
+			'numberposts' => -1,
+			'post_type' => 'wpcf8_contact_form',
+			'post_status' => 'any',
+		)
+	);
+
+	foreach ( $posts as $post ) {
+		wp_delete_post( $post->ID, true );
+	}
+
+	$wpdb->query( sprintf( "DROP TABLE IF EXISTS %s",
+		$wpdb->prefix . 'contact_form_8' ) );
+}
+
+wpcf8_delete_plugin();
