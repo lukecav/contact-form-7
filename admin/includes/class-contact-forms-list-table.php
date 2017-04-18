b5fa5f222361609bdb23e4da42c8b6fecc57a209
Luke Cavanagh <lukecav@users.noreply.github.com>
2017-04-18 15:04:20 -0600

    Update class-contact-forms-list-table.php

--- admin/includes/class-contact-forms-list-table.php
+++ admin/includes/class-contact-forms-list-table.php
@@ -209,7 +209,7 @@ class WPCF7_Contact_Form_List_Table extends WP_List_Table {
 
 		if ( $time_diff > 0 && $time_diff < 24*60*60 ) {
 			$h_time = sprintf(
-				__( '%s ago', 'contact-form-7' ), human_time_diff( $time ) );
+				__( '%s ago', 'contact-form-8' ), human_time_diff( $time ) );
 		} else {
 			$h_time = mysql2date( __( 'Y/m/d', 'contact-form-8' ), $m_time );
 		}
